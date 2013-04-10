89f757e6260b32cb123e55ddf86d650ee596e8d2
hoshit <hoshi@gclue.jp>
2013-04-10 15:14:14 +0900

    初回コミット.

--- gen/com/example/dentaku/R.java
+++ gen/com/example/dentaku/R.java
@@ -24,7 +24,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080010;
+        public static final int action_settings=0x7f080011;
+        public static final int answer=0x7f080010;
         public static final int button0=0x7f08000d;
         public static final int button1=0x7f080009;
         public static final int button2=0x7f08000a;
--- src/com/example/dentaku/Dentaku.java
+++ src/com/example/dentaku/Dentaku.java
@@ -1,8 +1,16 @@
 package com.example.dentaku;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -24,46 +32,45 @@ public class Dentaku extends Activity implements View.OnClickListener {
 	/**
 	 * キー
 	 */
-	private final int KEY_0 = 0;
-	private final int KEY_1 = 1;
-	private final int KEY_2 = 2;
-	private final int KEY_3 = 3;
-	private final int KEY_4 = 4;
-	private final int KEY_5 = 5;
-	private final int KEY_6 = 6;
-	private final int KEY_7 = 7;
-	private final int KEY_8 = 8;
-	private final int KEY_9 = 9;
 	private final int KEY_PLUS = 10;
 	private final int KEY_MINUS = 11;
 	private final int KEY_EQUAL = 12;
 	private final int KEY_TEN = 13;
 	private final int KEY_CLEAR = 14;
-
 	/**
 	 * TextView
 	 */
-	TextView mTextView;
+	private TextView mTextView;
+	private TextView mAnswer;
 
+	/**
+	 * 答えのリスト
+	 */
+	private String[] answers = new String[4];
 	/**
 	 * 前の処理
 	 */
-	int beforeStatus = 0;
+	private int beforeStatus = 0;
 
 	/**
-	 * 合計
+	 * 計算中の値
 	 */
-	int total = 0;
+	private ArrayList<String> calcArray;
+	/**
+	 * 計算する時の配列
+	 */
+	private ArrayList<String> signArray;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
 		// 表示用TextView
 		mTextView = (TextView) findViewById(R.id.display);
-
+		mAnswer = (TextView) findViewById(R.id.answer);
 		// Button
 		mButton = new Button[mId.length];
 
@@ -74,6 +81,8 @@ public class Dentaku extends Activity implements View.OnClickListener {
 			// buttonのイベント処理
 			mButton[i].setOnClickListener(this);
 		}
+		calcArray = new ArrayList<String>();
+		signArray = new ArrayList<String>();
 	}
 
 	@Override
@@ -82,15 +91,63 @@ public class Dentaku extends Activity implements View.OnClickListener {
 		// 押されたボタンがどのボタンかを判定
 		for (int i = 0; i < mId.length; i++) {
 			if (view.equals(mButton[i])) {
+				String nowValue = mTextView.getText().toString();
 				// CLEAR
 				if (i == KEY_CLEAR) {
 					mTextView.setText("");
-					total = 0;
+					calcArray.clear();
+					signArray.clear();
 					beforeStatus = KEY_CLEAR;
+				}
+				// =
+				else if (i == KEY_EQUAL && nowValue.length() > 0) {
+					nowValue = checkDisplay(nowValue);
+					calcArray.add(nowValue);
+					double ans = calc();
+					//テキストの文字を隠す
+					mTextView.setTextColor(Color.parseColor("#999999"));
+					showAlert(ans);
+					mTextView.setText(Double.toString(ans));
+					calcArray.clear();
+					signArray.clear();
+					beforeStatus = i;
+				}
+
+				// +
+				else if (i == KEY_PLUS && nowValue.length() > 0) {
+					calcArray.add(nowValue);
+					signArray.add("+");
+					beforeStatus = KEY_PLUS;
+				}
+				// -
+				else if (i == KEY_MINUS && nowValue.length() > 0) {
+					calcArray.add(nowValue);
+					signArray.add("-");
+					beforeStatus = KEY_MINUS;
+				}
+				// .
+				else if (i == KEY_TEN) {
+					// .キーを押した時、演算キーが押されていた場合
+					nowValue = checkDisplay(nowValue);
+					// いきなり.キーが押された場合
+					if (nowValue.length() == 0) {
+						nowValue = "0.";
+					} else {
+						nowValue = nowValue + ".";
+					}
+					mTextView.setText(nowValue);
+					beforeStatus = i;
 				}
 				// 数字
 				else if (i < 10) {
-					String nowValue = mTextView.getText().toString();
+					nowValue = checkDisplay(nowValue);
+					// 0しか入力されていない場合は0が２個以上並ばないようにする
+					if (nowValue.equals("0") && i == 0) {
+						return;
+					} else if (nowValue.equals("0") && i != 0) {
+						nowValue = "";
+					}
+
 					nowValue = nowValue + i;
 					mTextView.setText(nowValue);
 					beforeStatus = i;
@@ -98,5 +155,105 @@ public class Dentaku extends Activity implements View.OnClickListener {
 				break;
 			}
 		}
+	}
+	//演算が行われていた場合を確認する
+	//遅れた状態でディスプレイの値を初期化する
+	private String checkDisplay(String now) {
+		if (beforeStatus == KEY_PLUS 
+				|| beforeStatus == KEY_MINUS
+				|| beforeStatus == KEY_EQUAL) {
+			mTextView.setText("");
+			return "0";
+		}
+		return now;
+	}
+	// 計算する
+	private double calc() {
+		if (calcArray.size() == 0) {
+			return 0.0;
+		}
+		if (calcArray.size() == 1) {
+			return Double.parseDouble(calcArray.get(0));
+		}
+		double passive = Double.parseDouble(calcArray.get(0));
+		double active;
+		int j = 0;
+		for (int i = 1; i < calcArray.size(); i++) {
+			active = Double.parseDouble(calcArray.get(i));
+			if (signArray.get(j).equals("+")) {
+				passive += active;
+			} else {
+				passive -= active;
+			}
+			j++;
+		}
+		return passive;
+	}
+
+	/**
+	 * 選択のダイアログを表示する
+	 */
+	private void showAlert(double ans) {
+		//選択肢を生成する
+		Random random = new Random();
+		for (int i = 0; i < answers.length; i++) {
+			answers[i] = "";
+		}
+		//正解をどこにもってくるか
+		int success = (int) Math.abs(random.nextInt(4));
+		for (int i = 0; i < answers.length; i++) {
+			if (i == success) {
+				answers[i] = Double.toString(ans);
+				//間違った答えの中に同じものがないかどうか
+				for (int j = 0; j < answers.length; j++) {
+					if (i != j && !answers[j].equals("") && answers[i].equals(answers[j])) {
+						answers[i] = Double.toString(ans + 3);
+					}
+				}
+			} else {
+				double wrong;
+				boolean isWrong;
+				do {
+					isWrong = false;
+					wrong = ans + random.nextInt(10);
+					answers[i] = Double.toString(wrong);
+					//間違った答えの中に同じものがないかどうか
+					for (int j = 0; j < answers.length; j++) {
+						if (i != j && !answers[j].equals("") && answers[i].equals(answers[j])) {
+							isWrong = true;
+						}
+					}
+				} while (isWrong);
+				//正しい答えと同じものを生成しないためのループ
+			}
+		}
+		
+		MyDialogListener listener = new MyDialogListener();
+		AlertDialog.Builder myDialogBuilder = new AlertDialog.Builder(this);
+
+		myDialogBuilder.setTitle("答えは？").setItems(answers, listener)
+				.setCancelable(false);
+
+		AlertDialog myAlertDialog = myDialogBuilder.create();
+		myAlertDialog.show();
+	}
+
+	//回答一覧を管理するリスナー
+	private class MyDialogListener implements OnClickListener {
+
+		public void onClick(DialogInterface dialog, int which) {
+			//回答判定
+			if (mTextView.getText().toString().equals(answers[which])) {
+				mAnswer.setText("○");
+				mAnswer.setTextColor(Color.RED);
+			} else {
+				mAnswer.setText("×");
+				mAnswer.setTextColor(Color.BLUE);
+			}
+			//正解を表示
+			mTextView.setTextColor(Color.parseColor("#F39800"));
+
+		}
+
 	}
 }
